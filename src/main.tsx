1164993d936839e751787dbff759fc4e341174ec
小小指令猫 <164029689+xiumouer@users.noreply.github.com>
2025-12-13 16:40:51 +0800

    Delete src directory

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { AppRouterProvider } from './app/router'
-import './index.css'
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AppRouterProvider />
-  </React.StrictMode>
-)
